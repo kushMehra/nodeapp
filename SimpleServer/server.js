d456c9d3f77b078998bbb872e8f4bdd90e7505f6
kushMehra <kushagra.mehra@gmail.com>
2020-08-03 11:29:34 +1000

    Update server.js

--- SimpleServer/server.js
+++ SimpleServer/server.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1337;
@@ -9,4 +9,15 @@ http.createServer(function(req,res) {
   res.end(data);
 });
 }).listen(port, hostname);
-console.log('Server running at http://' + hostname + ':' + port + '/');
+console.log('Server running at http://' + hostname + ':' + port + '/');*/
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
